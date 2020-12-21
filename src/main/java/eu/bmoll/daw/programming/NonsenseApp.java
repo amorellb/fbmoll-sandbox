7c7a9ca61e70f191f719b7c50e47991124f26f04
Antonio Morell <amorellb@cifpfbmoll.eu>
2020-12-21 20:32:05 +0100

    feat: console input test

---
+++ src/main/java/eu/bmoll/daw/programming/NonsenseApp.java
@@ -0,0 +1,18 @@
+package eu.bmoll.daw.programming;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class NonsenseApp {
+    public static void main(String[] args) {
+        boolean aux = StringUtils.equals("a", "A");
+        System.out.println("Hola Mundo: " + aux);
+        // Using Scanner for Getting Input from User
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        System.out.println("You entered string " + s);
+        System.out.println("-END-");
+
+    }
+}
