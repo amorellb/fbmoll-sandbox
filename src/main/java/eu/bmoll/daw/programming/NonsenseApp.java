9167fe398e24da44c226b4c01be6e95327301fc3
Antonio Morell <amorellb@cifpfbmoll.eu>
2020-12-21 20:52:21 +0100

    feat: console input test

--- src/main/java/eu/bmoll/daw/programming/NonsenseApp.java
+++ src/main/java/eu/bmoll/daw/programming/NonsenseApp.java
@@ -1,17 +1,24 @@
 package eu.bmoll.daw.programming;
 
+import eu.bmoll.daw.programming.helper.ConsoleUtils;
 import org.apache.commons.lang3.StringUtils;
-
 import java.util.Scanner;
 
 public class NonsenseApp {
+    private static  final String EXIT_KEY = "9";
+
     public static void main(String[] args) {
         boolean aux = StringUtils.equals("a", "A");
         System.out.println("Hola Mundo: " + aux);
-        // Using Scanner for Getting Input from User
-        Scanner in = new Scanner(System.in);
-        String s = in.nextLine();
-        System.out.println("You entered string " + s);
+
+        ConsoleUtils consoleUtils = new ConsoleUtils();
+        boolean isExit = false;
+        while (!isExit) {
+            String s = consoleUtils.getUserInput();
+            System.out.println("You entered string " + s);
+            isExit = StringUtils.equals(s, EXIT_KEY);
+        }
+
         System.out.println("-END-");
 
     }
